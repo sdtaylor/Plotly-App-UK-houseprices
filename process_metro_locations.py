59d7c26cde1df9ce02234c57ca79f816ee566be9
Shawn <shawntaylor@weecology.org>
2024-03-02 09:33:45 -0700

    make metros a star shape

--- process_metro_locations.py
+++ process_metro_locations.py
@@ -13,6 +13,53 @@ from config import config as cfg
 
 redfin_metro_info = get_all_region_info(return_mapping=False).query("region_type=='metro'")
 
+
+    
+from shapely_extra.angles import point_from_angle_and_distance
+from shapely.geometry import Polygon
+
+def star(center_point = (0,0), outer_radius=10, inner_radius=10/3, inner_radius_ratio=None):
+    """ 
+    Create 5 point star shape centered on center_point.
+    An inner point ratio between 0.3 and 0.4 makes decent looking stars
+    
+    outer_radius: radius of the  outer points
+    inner_radius: radius of the inner points, ignored if inner_radius_ratio is set
+    inner_radius_ratio: ratio of the inner to outer radius. 
+    
+    """
+    if inner_radius_ratio is not None:
+        if inner_radius_ratio >= 1 or inner_radius_ratio <= 0:
+            raise ValueError('inner_radius_ratio must be between 0-1')
+        inner_radius = outer_radius * inner_radius_ratio
+
+    #center_point = (0,0)
+    #outer_radius = 5
+    #inner_radius_ratio = 1/2.5
+    #inner_radius = outer_radius * inner_radius_ratio
+    
+    # coordinates from https://math.stackexchange.com/a/3582484
+    point_radi = [
+        (outer_radius, 18),
+        (inner_radius, 54),
+        (outer_radius, 90),
+        (inner_radius, 126),
+        (outer_radius, 162),
+        (inner_radius, 198),
+        (outer_radius, 235),
+        (inner_radius, 270),
+        (outer_radius, 306),
+        (inner_radius, 342),
+        ]
+    
+    
+    points = [
+        point_from_angle_and_distance(ref_point=center_point, angle= a, distance = d, use_radians=False)
+            for d, a in point_radi
+        ]
+    
+    return Polygon(points)
+
 def round_coordinates(geom, ndigits=2):
     
    def _round_coords(x, y, z=None):
@@ -67,13 +114,27 @@ city_gdf = city_gdf.merge(redfin_metro_info[['region_name','region_id']], how='l
 point_geofile = cfg['assets dir'].joinpath('original_metro_points.geojson')
 city_gdf.to_file(point_geofile, driver='GeoJSON')
 
-city_gdf['geometry'] = city_gdf.geometry.buffer(0.2)
+city_gdf = gpd.read_file(point_geofile)
 
-city_gdf['geometry'] = city_gdf.geometry.apply(round_coordinates, ndigits=2)
+star_shapes = city_gdf.copy().to_crs('EPSG:3857')
 
-city_json = city_gdf.__geo_interface__
+star_diameter = 30 * 1000 # 30km
+star_inner_ratio = 0.4
+
+make_star = lambda g: star(center_point=g.centroid, outer_radius=star_diameter, inner_radius_ratio = star_inner_ratio)
+star_shapes['geometry'] = star_shapes.geometry.apply(make_star)
+
+star_shapes = star_shapes.to_crs('EPSG:4326')
+
+star_shapes['geometry'] = star_shapes.geometry.apply(round_coordinates, ndigits=2)
+
+city_json = star_shapes.__geo_interface__
 
 dst_file = cfg['assets dir'].joinpath(cfg['geodata_files']['metros'])
 
 with open(dst_file,'w') as f:
     json.dump(city_json, f, separators=(',', ':'))
+    
+
+
+
